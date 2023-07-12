6204078bb37d6f46975e3e5be66bbc5a50afa1e2
Ichiya Luna <ichiyaluna@yonsei.ac.kr>
2023-07-13 00:01:04 +0900

    :heavy_plus_sign: SASS 추가
    - dev dependency 추가: SASS
    - next.config.js 수정: SASS 지원 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// Requires
+const path = require('path');
+// Config
+const nextConfig = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
